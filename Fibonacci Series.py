dfa5f2fc715d75fb4754f92ef5b3c753abe016d2
yadhugowda <122009314+yadhugowda@users.noreply.github.com>
2023-01-14 21:30:34 +0530

    Update Fibonacci Series.py

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,5 +1,12 @@
-x,y=0,1
-
-while y<50:
-    print(y)
-    x,y = y,x+y
+n=int(input("Enter the number of terms: "))
+a=0
+b=1
+if n<=0:
+    print("The Output of your input is",a)
+else:
+    print(a,b,end=" ")
+    for x in range(2,n):
+        c=a+b
+        print(c,end=" ")
+        a=b
+        b=c
